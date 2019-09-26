4d81fd9e4a87650dca8a86c6f31ca1e694cecbb3
Aleksandr Alekseev <hiddenfox29@yandex.ru>
2019-09-26 19:34:02 +0600

    learn JS

---
+++ app.js
@@ -0,0 +1,27 @@
+let pswd      = "qwert1234";
+let numberWin = 29;
+
+// вход через пароль
+let answer = prompt('Введите пароль');
+if (pswd === answer) {
+    document.write('secret content')
+} else {
+    alert('Увы')
+};
+
+// игра
+let answerNumber = Number(prompt('Введите число'));
+
+if (numberWin === answerNumber) {
+    alert("win")
+};
+
+if (numberWin < answerNumber){
+    alert('Меньше');
+};
+
+if(numberWin > answerNumber) {
+    alert('Больше');
+};
+
+
